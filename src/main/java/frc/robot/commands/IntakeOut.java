a17449e3ebff3bfb68f2b91af398fc3f6f44524a
sat-malla <57183091+sat-malla@users.noreply.github.com>
2025-02-26 00:39:43 -0800

    Adding intake commands

--- src/main/java/frc/robot/commands/IntakeOut.java
+++ src/main/java/frc/robot/commands/IntakeOut.java
@@ -4,23 +4,17 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralIntake;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends Command {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+public class IntakeOut extends Command {
+  CoralIntake coralIntake;
+  double power = 0.0;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  public IntakeOut(CoralIntake coralIntake, double power) {
+    this.coralIntake = coralIntake;
+    this.power = power;
+    addRequirements(coralIntake);
   }
 
   // Called when the command is initially scheduled.
@@ -29,11 +23,15 @@ public class ExampleCommand extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    coralIntake.intakeRun(-power);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    coralIntake.stop();
+  }
 
   // Returns true when the command should end.
   @Override
